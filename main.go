f9d6551d8a94dce92b7dbde9fe084b0cb618084c
admin <js.byeon@teamsparta.co>
2024-02-05 17:49:23 +0900

    make basic props

--- main.go
+++ main.go
@@ -1,43 +1,74 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
-	"multiplayer_server/protodef"
 	"net"
-
-	"google.golang.org/protobuf/proto"
+	"net/http"
 )
 
+const WORKER_COUNT int = 10;
+type ChannelAndPort struct {
+	Channel chan int
+	Port int
+}
 func main() {
-	// Go에서 protobuf를 사용하기 위해 필요한 단계: https://protobuf.dev/getting-started/gotutorial/
-	// ex) protoc --go_out=$PWD proto/status.proto
-	addr, err := net.ResolveUDPAddr("udp", ":8888")
+	workerLines := make([]ChannelAndPort, 0, WORKER_COUNT)
 
-	if err != nil {
-		panic(err)
-	}
-
-	conn, err := net.ListenUDP("udp", addr)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer conn.Close()
-
-	for {
-		buffer := make([]byte, 1024)
-		amount, _, err := conn.ReadFromUDP(buffer)
+	for workerId := 0; workerId < WORKER_COUNT; workerId++  {
+		workerLine := make(chan int)
+		addr, err := net.ResolveUDPAddr("udp", ":0")
 
 		if err != nil {
-			log.Fatal(err.Error())
+			panic(err)
 		}
-
-		status := new(protodef.Status)
-		desErr := proto.Unmarshal(buffer[:amount], status)
-
-		if desErr != nil {
-			log.Fatal(err.Error())
+	
+		conn, err := net.ListenUDP("udp", addr)
+	
+		if err != nil {
+			panic(err)
 		}
+	
+		port := conn.LocalAddr().(*net.UDPAddr).Port
+		
+		go func(workerId int, conn *net.UDPConn, workerLine <-chan int) {
+			defer conn.Close()
+
+			for message := range workerLine {
+				fmt.Println(message)
+			}
+		}(workerId, conn, workerLine)
+
+		workerLines = append(workerLines, ChannelAndPort{
+			Channel: workerLine,
+			Port: port,
+		})
 	}
+	// Go에서 protobuf를 사용하기 위해 필요한 단계: https://protobuf.dev/getting-started/gotutorial/
+	// ex) protoc --go_out=$PWD proto/status.proto
+	http.HandleFunc("/get-worker-port", func(w http.ResponseWriter, r *http.Request) {
+
+
+		w.Header().Set("Content-Type", "text/plain")
+		io.WriteString(w, fmt.Sprintf("%d", port))
+	})
+
+	log.Fatal(http.ListenAndServe(":8888", nil))
+
+	// for {
+	// 	buffer := make([]byte, 1024)
+	// 	amount, _, err := conn.ReadFromUDP(buffer)
+
+	// 	if err != nil {
+	// 		log.Fatal(err.Error())
+	// 	}
+
+	// 	status := new(protodef.Status)
+	// 	desErr := proto.Unmarshal(buffer[:amount], status)
+
+	// 	if desErr != nil {
+	// 		log.Fatal(err.Error())
+	// 	}
+	// }
 }
