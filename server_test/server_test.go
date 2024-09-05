025963cd58701ac9340a9fbb2c9c60f509628da4
jsbyeon1 <js.byeon@teamsparta.co>
2024-09-06 02:28:02 +0900

    divide test to independent module

--- server_test/server_test.go
+++ server_test/server_test.go
@@ -1,11 +1,9 @@
-package http_server
+package server_test
 
 import (
 	"coin_chase/game"
-	"coin_chase/protodef"
+	"coin_chase/http_server"
 	"strings"
-	"sync"
-	"time"
 
 	"encoding/json"
 	"fmt"
@@ -16,9 +14,9 @@ import (
 	"os"
 	"strconv"
 	"testing"
-
-	"google.golang.org/protobuf/proto"
 )
+
+// run test with "go test ./server_test -v -race"
 
 // 프로그램의 random배치 특성상 유저의 정확한 순위 등을 예측하기는 어렵다.
 // 프로젝트의 목적 자체에 가까운 high throughput, small packet size, synchronization등을 테스트한다.
@@ -43,7 +41,7 @@ var (
 
 func TestMain(m *testing.M) {
 	clientListeners = make([]*TestClient, 0)
-	httpServer := NewServer(10, 10)
+	httpServer := http_server.NewServer(10, 10)
 	testServer = httptest.NewServer(httpServer)
 
 	defer testServer.Close()
@@ -155,73 +153,73 @@ func TestWorkerPullOut(t *testing.T) {
 	})
 }
 
-func TestPlay(t *testing.T) {
-	// 유저의 랜덤 위치 이동(스코어가 쌓일 시간을 주기 위해 10초 실행. 복수의 유저가 크래시 없이 game map과 scoreboard에 write하고 점수를 쌓는 것 자체가 테스트의 목적)
-	// 정확한 데이터는 예측할 수 없다. 랜덤으로 동전들이 움직일 뿐 더러, 유저의 이동도 랜덤이기 때문에 모두가 0점일 수도 있고 점수가 같거나 높을 수도 있다.
-	t.Run("유저의 랜덤 위치 이동", func(t *testing.T) {
-		if testing.Short() {
-			t.Skip("skipping test in short mode.")
-		}
+// func TestPlay(t *testing.T) {
+// 	// 유저의 랜덤 위치 이동(스코어가 쌓일 시간을 주기 위해 10초 실행. 복수의 유저가 크래시 없이 game map과 scoreboard에 write하고 점수를 쌓는 것 자체가 테스트의 목적)
+// 	// 정확한 데이터는 예측할 수 없다. 랜덤으로 동전들이 움직일 뿐 더러, 유저의 이동도 랜덤이기 때문에 모두가 0점일 수도 있고 점수가 같거나 높을 수도 있다.
+// 	t.Run("유저의 랜덤 위치 이동", func(t *testing.T) {
+// 		if testing.Short() {
+// 			t.Skip("skipping test in short mode.")
+// 		}
 
-		var allClientWaitGroup sync.WaitGroup
-		timeoutReached := false
-		overallTimeLimit := 10
+// 		var allClientWaitGroup sync.WaitGroup
+// 		timeoutReached := false
+// 		overallTimeLimit := 10
 
-		go func() {
-			time.Sleep(time.Second * time.Duration(overallTimeLimit))
-			timeoutReached = true
-		}()
+// 		go func() {
+// 			time.Sleep(time.Second * time.Duration(overallTimeLimit))
+// 			timeoutReached = true
+// 		}()
 
-		for _, testClient := range clientListeners {
-			allClientWaitGroup.Add(1)
-			go func() {
-				userPosition := &protodef.Position{
-					X: 0,
-					Y: 0,
-				}
+// 		for _, testClient := range clientListeners {
+// 			allClientWaitGroup.Add(1)
+// 			go func() {
+// 				userPosition := &protodef.Position{
+// 					X: 0,
+// 					Y: 0,
+// 				}
 
-				for !timeoutReached {
-					// ! 중요 !
-					// 원래대로라면 서버가 클라이언트의 위치를 강제로 결정할 수 있다(클라이언트가 유효하지 않은 위치에 존재할 시).
-					// 그래서 서버가 보내주는 데이터를 토대로 클라이언트에서 유저의 위치를 재조정하는 등의 동작이 가능하지만럼(멀티 플레이 게임에서 렉 걸릴 때 캐릭터 위치 강제로 결정되는 것 처럼)
-					// 이를 구현하려면 클라이언트에 구현되어 있는 것 처럼,
-					// 각 테스트 클라이언트가 서버의 TCP 통신을 수신하고 다시 그에 따라 위치를 position을 결정하는
-					// 클라이언트의 로직이 서버 테스트에 포함되게 되므로, 서버의 테스트가 아닌 클라이언트의 테스트가 되어버린다.
-					// 서버가 애초에 out of bound 혹은 이미 점거된 위치에 대한 판단을 서버가 알아서 진행하므로, 유효하지 않은 위치를 계산하지 않고
-					// 클라이언트의 랜덤 포지션을 그대로 전송한다. 이 방법으로도 데이터에 대한 경합성 해결, 요청 무효화 등의 테스트는 가능하다.
-					newUserPosition := &protodef.Position{
-						X: userPosition.X + game.GenerateRandomDirection(),
-						Y: userPosition.Y + game.GenerateRandomDirection(),
-					}
+// 				for !timeoutReached {
+// 					// ! 중요 !
+// 					// 원래대로라면 서버가 클라이언트의 위치를 강제로 결정할 수 있다(클라이언트가 유효하지 않은 위치에 존재할 시).
+// 					// 그래서 서버가 보내주는 데이터를 토대로 클라이언트에서 유저의 위치를 재조정하는 등의 동작이 가능하지만럼(멀티 플레이 게임에서 렉 걸릴 때 캐릭터 위치 강제로 결정되는 것 처럼)
+// 					// 이를 구현하려면 클라이언트에 구현되어 있는 것 처럼,
+// 					// 각 테스트 클라이언트가 서버의 TCP 통신을 수신하고 다시 그에 따라 위치를 position을 결정하는
+// 					// 클라이언트의 로직이 서버 테스트에 포함되게 되므로, 서버의 테스트가 아닌 클라이언트의 테스트가 되어버린다.
+// 					// 서버가 애초에 out of bound 혹은 이미 점거된 위치에 대한 판단을 서버가 알아서 진행하므로, 유효하지 않은 위치를 계산하지 않고
+// 					// 클라이언트의 랜덤 포지션을 그대로 전송한다. 이 방법으로도 데이터에 대한 경합성 해결, 요청 무효화 등의 테스트는 가능하다.
+// 					newUserPosition := &protodef.Position{
+// 						X: userPosition.X + game.GenerateRandomDirection(),
+// 						Y: userPosition.Y + game.GenerateRandomDirection(),
+// 					}
 
-					userStatus := &protodef.Status{
-						Id:              testClient.ID,
-						CurrentPosition: newUserPosition,
-					}
+// 					userStatus := &protodef.Status{
+// 						Id:              testClient.ID,
+// 						CurrentPosition: newUserPosition,
+// 					}
 
-					userPosition = newUserPosition
-					marshaledData, err := proto.Marshal(userStatus)
+// 					userPosition = newUserPosition
+// 					marshaledData, err := proto.Marshal(userStatus)
 
-					if err != nil {
-						panic(err)
-					}
-					// 유저의 위치를 서버에 전송
-					_, err = testClient.Conn.Write(marshaledData)
+// 					if err != nil {
+// 						panic(err)
+// 					}
+// 					// 유저의 위치를 서버에 전송
+// 					_, err = testClient.Conn.Write(marshaledData)
 
-					if err != nil {
-						t.Log(err)
-					}
+// 					if err != nil {
+// 						t.Log(err)
+// 					}
 
-					time.Sleep(time.Millisecond * 200)
-				}
+// 					time.Sleep(time.Millisecond * 200)
+// 				}
 
-				allClientWaitGroup.Done()
-			}()
+// 				allClientWaitGroup.Done()
+// 			}()
 
-			allClientWaitGroup.Wait()
-		}
-	})
-}
+// 			allClientWaitGroup.Wait()
+// 		}
+// 	})
+// }
 
 func TestWorkerPutBack(t *testing.T) {
 	t.Run("로그아웃(워커 반환)", func(t *testing.T) {
